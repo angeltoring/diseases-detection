2c00a5c91e37e1647857e4e77ea6dec86aed552d
keshavsharma98 <ks98eng@gmail.com>
2023-07-24 06:03:50 +0530

    init

--- api/index.py
+++ api/index.py
@@ -1,11 +1,53 @@
-from flask import Flask
+import os
+import time
+from flask import Flask, send_file, request, jsonify
+import subprocess
+import shutil
+from yolomodel.main import model_main
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+def capture_image():
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    image_path = f"/home/orangepi/Desktop/capture_fswebcam/captured/image_{timestamp}.jpg"
+    skip_frames = 20
+    command = f"fswebcam -r 1920x1080 --no-banner -S {skip_frames} {image_path}"
+    subprocess.run(command, shell=True, check=True)
+    return image_path
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/capture', methods=['GET'])
+def capture_and_send_image():
+    image_path = capture_image()
+    return send_file(image_path, mimetype="image/jpeg")
+
+@app.route('/', methods=['GET'])
+def check():
+    return "SUCCESS"
+
+@app.route('/detect', methods=['POST'])
+def process_image():
+    
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+    
+    try:
+        # Clear the images folder if it exists, otherwise create it
+        image_folder = 'captured'
+        
+        if os.path.exists(image_folder):
+            shutil.rmtree(image_folder)
+        os.makedirs(image_folder)
+
+        file_path = os.path.join(image_folder,'req-image.jpg')
+        file.save(file_path)
+
+        model_main()
+
+        # Return a success response
+        return jsonify({'success': 'Image processed successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
