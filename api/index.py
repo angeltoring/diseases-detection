5398f513f97f4c575280ece357276e6266c7b848
keshavsharma98 <ks98eng@gmail.com>
2023-07-24 06:07:38 +0530

    edit

--- api/index.py
+++ api/index.py
@@ -7,19 +7,6 @@ from yolomodel.main import model_main
 
 app = Flask(__name__)
 
-def capture_image():
-    timestamp = time.strftime("%Y%m%d_%H%M%S")
-    image_path = f"/home/orangepi/Desktop/capture_fswebcam/captured/image_{timestamp}.jpg"
-    skip_frames = 20
-    command = f"fswebcam -r 1920x1080 --no-banner -S {skip_frames} {image_path}"
-    subprocess.run(command, shell=True, check=True)
-    return image_path
-
-@app.route('/capture', methods=['GET'])
-def capture_and_send_image():
-    image_path = capture_image()
-    return send_file(image_path, mimetype="image/jpeg")
-
 @app.route('/', methods=['GET'])
 def check():
     return "SUCCESS"
