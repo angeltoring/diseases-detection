a4a4186c2a0369b7ed93f8cc6e686fa88eccb6da
keshavsharma98 <ks98eng@gmail.com>
2024-01-08 00:30:09 +0530

    fix 3

--- yolomodel/yolov5/detect.py
+++ yolomodel/yolov5/detect.py
@@ -36,6 +36,8 @@ from pathlib import Path
 import datetime
 import torch
 import numpy as np
+import subprocess
+subprocess.run(["pip", "install", "ultralytics"], check=True)
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
